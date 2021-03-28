0cf5076ddcca44c8ca7cbc2836acd0c8875c758d
Ziad Ali <ziadalikhalifa@gmail.com>
2021-03-28 22:58:23 +0200

    Remove the default React test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
 test('Mock test to check CICD', () => {
   const x = 1;
   expect(x).toBe(1);
