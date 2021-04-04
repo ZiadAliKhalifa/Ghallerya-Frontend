33e5f5ac38d2413f91ca2f9d491f90202535e27b
Ziad Ali <ziadalikhalifa@gmail.com>
2021-04-04 12:21:59 +0200

    Updated redirect address

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ ReactDOM.render(
     <Auth0Provider
       domain={process.env.REACT_APP_AUTH0_DOMAIN || ""}
       clientId={process.env.REACT_APP_AUTH0_CLIENT_ID || ""}
-      redirectUri={window.location.origin}
+      redirectUri={window.location.origin + window.location.pathname}
     >
       <App />
     </Auth0Provider>
