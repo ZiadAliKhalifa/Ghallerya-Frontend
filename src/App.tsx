11bcd1325e8c75e96defb0f3532c0ceb652766ca
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2021-04-06 03:53:54 +0200

    created the navbar component, still working on it though

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import Homepage from "./components/Homepage/Homepage";
 
-import logo from './logo.svg';
-import './App.css';
-import LoginButton from "./components/Auth/LoginButton"
-import LogoutButton from "./components/Auth/LogoutButton"
-import SetUserInRedux from "./components/Auth/SetUserInRedux"
+import logo from "./logo.svg";
+import "./App.css";
+import LoginButton from "./components/Auth/LoginButton";
+import LogoutButton from "./components/Auth/LogoutButton";
+import SetUserInRedux from "./components/Auth/SetUserInRedux";
 
 function App() {
   const { user, isAuthenticated } = useAuth0();
@@ -13,11 +14,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Homepage />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>Hi, this is Hedwig in TS!</p>
         <p>You are on the {process.env.NODE_ENV} environment</p>
-        {isAuthenticated && (<p>You are authenticated as: {user.name}!</p>)}
-        {!isAuthenticated && (<p>You are not authenticated.</p>)}
+        {isAuthenticated && <p>You are authenticated as: {user.name}!</p>}
+        {!isAuthenticated && <p>You are not authenticated.</p>}
         <LoginButton />
         <LogoutButton />
         <SetUserInRedux />
--- src/components/Homepage/Homepage.tsx
+++ src/components/Homepage/Homepage.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 export default function Homepage() {
-    return (
-        <div>
-            {/* TODO: Place all components that should be in Homepage here */}
-        </div>
-    )
+  return (
+    <div>
+      {/* TODO: Place all components that should be in Homepage here */}
+      <Navbar />
+    </div>
+  );
 }
