4349fc17e3bfb1af2a486a119c84981268835750
Ziad Ali <ziadalikhalifa@gmail.com>
2021-04-12 00:06:07 +0200

    Added megatron with image and header

---
+++ src/components/Homepage/Megatron/Megatron.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Megatron.css';
+
+import Belal from "./../../../static/images/Belal.png"
+
+function Megatron() {
+    return (
+        <div className="megatron">
+            <img src={Belal} alt="An old guy smilling" />
+            <h1>Find the perfect image</h1>
+        </div>
+    )
+}
+
+export default Megatron
