5310e0788348f16702b0b1e5a8fa00811abca4cc
Ziad Ali <ziadalikhalifa@gmail.com>
2021-04-12 00:05:50 +0200

    Cleaned up app and added megatron to homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import { useAuth0 } from "@auth0/auth0-react";
 
-import logo from './logo.svg';
-import './App.css';
-import LoginButton from "./components/Auth/LoginButton"
-import LogoutButton from "./components/Auth/LogoutButton"
-import SetUserInRedux from "./components/Auth/SetUserInRedux"
+import Homepage from './components/Homepage/Homepage';
 
 function App() {
-  const { user, isAuthenticated } = useAuth0();
+
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi, this is Hedwig in TS!</p>
-        <p>You are on the {process.env.NODE_ENV} environment</p>
-        {isAuthenticated && (<p>You are authenticated as: {user.name}!</p>)}
-        {!isAuthenticated && (<p>You are not authenticated.</p>)}
-        <LoginButton />
-        <LogoutButton />
-        <SetUserInRedux />
-      </header>
+      <Homepage />
     </div>
   );
 }
