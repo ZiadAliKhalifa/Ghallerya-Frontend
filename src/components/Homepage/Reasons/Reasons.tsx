5f50736c28b120fc0a0cadbead83179bdd445c25
Ziad Ali <ziadalikhalifa@gmail.com>
2021-04-21 00:27:00 +0200

    Added the "Why use Ghallerya" segment

---
+++ src/components/Homepage/Reasons/Reasons.tsx
@@ -0,0 +1,30 @@
+import photographer from "../../../static/images/photographer.svg"
+import camel from "../../../static/images/camel.svg"
+import payments from "../../../static/images/payments.svg"
+import "./Reasons.css"
+
+const Reasons = () => {
+    return (
+        <div className="reasons">
+            <h1 >Why use Ghallerya</h1>
+            <div className="image-area">
+                <div className="image-wrapper">
+                    <img src={photographer} alt="Illustration of a girl seeing a flower through her camera lens" />
+                    <p>Empowering photographers</p>
+                </div>
+                <div className="image-wrapper">
+                    <img src={camel} alt="Illustration of a girl riding a camel in the dessert" />
+                    <p>Middle-eastern focused</p>
+                </div>
+                <div className="image-wrapper">
+                    <img src={payments} alt="Illustration of a guy making it rain with money" />
+                    <p>Ultra-secure payments</p>
+                </div>
+            </div>
+
+        </div >
+    )
+
+}
+
+export default Reasons;
