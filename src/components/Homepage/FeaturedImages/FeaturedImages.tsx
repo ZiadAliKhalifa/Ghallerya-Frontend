adebce66ddb106ccc6a3809ab301a5d4d35c1367
Ziad Ali <ziadalikhalifa@gmail.com>
2021-04-21 02:20:24 +0200

    Added Featured images layout

---
+++ src/components/Homepage/FeaturedImages/FeaturedImages.tsx
@@ -0,0 +1,27 @@
+import "./FeaturedImages.css"
+
+const FeaturedImages = () => {
+    return (
+        <div className="featured-pics">
+            <span className="header">Featured images</span>
+            <div className="image-grid">
+                <div className="image-area">
+                    <div className="featured-pic" />
+                    <div className="featured-pic" />
+                    <div className="featured-pic" />
+                    <div className="featured-pic" />
+                    <div className="featured-pic" />
+                    <div className="featured-pic" />
+                    <div className="featured-pic extra" />
+                    <div className="featured-pic extra" />
+                    <div className="featured-pic extra" />
+                    <div className="featured-pic extra" />
+                    <div className="featured-pic extra" />
+                    <div className="featured-pic extra" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedImages;
